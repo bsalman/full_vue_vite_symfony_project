7e256bf174a620e0ded6ab588008ea32fbc0dbf9
bsalman <bsalman395850@gmail.com>
2024-10-30 18:12:13 +0100

    tailwind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,48 +1,27 @@
+<template>
+  <div
+    class="container max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl bg-gray-100">
+    <div class="text-3xl font-bold underline">hallo</div>
+  </div>
+</template>
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
 import { onMounted, ref } from "vue";
 import axios from "axios";
 
-onMounted(async () => {
-  await axios.get("/api/test").then((response) => {
-    console.log(response);
-  });
-  await axios
-    .post("/api/form", {
-      firstName: "Fred",
-      lastName: "Flintstone"
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-});
+// onMounted(async () => {
+//   await axios.get("/api/test").then((response) => {
+//     console.log(response);
+//   });
+//   await axios
+//     .post("/api/form", {
+//       firstName: "Fred",
+//       lastName: "Flintstone"
+//     })
+//     .then(function (response) {
+//       console.log(response);
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+// });
 </script>
-
-<template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-</template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
